9f0fd0b3fca73890a86fd5176e70b6a857713c54
Izzr4 <112739379+Izzr4@users.noreply.github.com>
2023-03-03 18:33:36 +0700

    Update main.go

--- main.go
+++ main.go
@@ -19,6 +19,19 @@ func main() {
 	//	log.Fatalln("failed to load env file")
 	//}
 	r := gin.Default()
+	r.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		if c.Request.Method == "OPTIONS" {
+			c.Writer.Header().Set("Content-Type", "application/json")
+			c.AbortWithStatus(204)
+		} else {
+			c.Next()
+		}
+	})
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"ping": "pong",
@@ -33,18 +46,6 @@ func main() {
 	v1.POST("/login", Handler.LogIn)
 	v1.GET("/validate", middleware.Auth, Handler.Validate)
 	//v1.POST("/changePass", Handler.ChangePass)
-	r.Use(func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		if c.Request.Method == "OPTIONS" {
-			c.Writer.Header().Set("Content-Type", "application/json")
-			c.AbortWithStatus(204)
-		} else {
-			c.Next()
-		}
-	})
+	
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
