a80ab6f23faca87c0d2d7c9aa3243199eb9ef12f
izra4 <aryaizra2@gmail.com>
2023-03-10 10:12:18 +0700

    DEPLOY booking api

--- main.go
+++ main.go
@@ -24,7 +24,6 @@ func main() {
 	}
 	r := gin.Default()
 	r.Use(func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, authorization, accept, origin, Cache-Control, X-Requested-With")
