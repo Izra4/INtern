d8c127d1e8c27f3dc43673fecfcdafacdf1696aa
izra4 <aryaizra2@gmail.com>
2023-03-13 20:55:57 +0700

    DEPLOY test book

--- entity/booking.go
+++ entity/booking.go
@@ -8,7 +8,7 @@ import (
 type Booking struct {
 	gorm.Model
 	Nama      string    `gorm:"type:VARCHAR(50);NOT NULL"`
-	Tanggal   time.Time `gorm:"type:time;NOT NULL"`
+	Tanggal   time.Time `gorm:"type:time;NOT NULL" type:"date"`
 	Keperluan string    `gorm:"type:VARCHAR(30);NOT NULL'"`
 	Nomer     string    `gorm:"type:VARCHAR(15);NOT NULL"`
 	Alamat    string    `gorm:"type:VARCHAR(200);NOT NULL"`
